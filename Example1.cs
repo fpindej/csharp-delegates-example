0599632201b4aad062fe68c58c952eec96b68980
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2021-08-30 22:45:05 +0200

    Refactor Example1

--- Example1.cs
+++ Example1.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 
-internal class Example1
+namespace DelegatesExample
 {
-    private delegate int Calculate(int x, int y);
-
-    private static int Add(int x, int y) => x + y;
-
-    private static int Subtract(int x, int y) => x - y;
-
-    public static void Run()
+    internal class Example1
     {
-        var addOperation = new Calculate(Add);
-        var subtractOperation = new Calculate(Subtract);
-        Calculate multiplyOperation = (x, y) => x * y;
+        private delegate int Calculate(int x, int y);
 
-        Console.WriteLine("Sum of 5 and 3 is ", addOperation(5, 3));
-        Console.WriteLine("Subtraction of 420 and 69 is ", subtractOperation(420, 69));
-        Console.WriteLine("Multiplication of 2 and 2 is ", multiplyOperation(2, 2));
+        private static int Add(int x, int y) => x + y;
+
+        private static int Subtract(int x, int y) => x - y;
+
+        internal static void Run()
+        {
+            var addOperation = new Calculate(Add);
+            var subtractOperation = new Calculate(Subtract);
+            Calculate multiplyOperation = (x, y) => x * y;
+
+            Console.WriteLine("Sum of 5 and 3 is ", addOperation(5, 3));
+            Console.WriteLine("Subtraction of 420 and 69 is ", subtractOperation(420, 69));
+            Console.WriteLine("Multiplication of 2 and 2 is ", multiplyOperation(2, 2));
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
-﻿using System;
-
-namespace DelegatesExample
+﻿namespace DelegatesExample
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             Example1.Run();
         }
