61221469b14729e13db05fea6e62c4e5e472a5a2
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2021-08-30 23:16:46 +0200

    Improve naming in Example2

--- Example2.cs
+++ Example2.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 
 namespace DelegatesExample
 {
-    internal delegate bool IsPassedExam(int totalMarks);
+    internal delegate bool IsPassedExam(int totalScore);
 
     internal class Student
     {
-        private readonly int _totalMarks;
+        private readonly int _totalScore;
 
         private string Name { get; set; }
 
-        internal Student(string name, int totalMarks)
+        internal Student(string name, int totalScore)
         {
             Name = name;
-            _totalMarks = totalMarks;
+            _totalScore = totalScore;
         }
 
         internal void CheckIfPassed(IsPassedExam isPassedExam)
         {
-            if (isPassedExam(_totalMarks))
+            if (isPassedExam(_totalScore))
             {
                 Console.WriteLine($"Student {Name} passed the exam");
             }
@@ -32,16 +32,16 @@ namespace DelegatesExample
 
     internal class Example2
     {
-        private static bool ConditionForPassing(int marks) => marks > 50;
+        private static bool ConditionForPassing(int score) => score > 50;
 
         internal static void Run()
         {
             var students = new List<Student>
             {
-                new Student("Hari",90),
-                new Student("Tom",98),
-                new Student("John",46),
-                new Student("James",49)
+                new Student("Hari", 90),
+                new Student("Tom", 98),
+                new Student("John", 46),
+                new Student("James", 49)
             };
 
             var isPassedExam = new IsPassedExam(ConditionForPassing);
