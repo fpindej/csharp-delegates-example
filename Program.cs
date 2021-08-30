2fc8ad0cd30903188086135289e4c8da4bf3ba2f
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2021-08-30 23:30:41 +0200

    Use delegate in Main to improve the overall example

--- Program.cs
+++ Program.cs
@@ -1,16 +1,21 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace DelegatesExample
 {
     internal class Program
     {
+        public delegate void RunDelegate();
+
+        public delegate Task RunDelegateAsync();
+
         private static async Task Main()
         {
-            //Example1.Run();
-            //Example2.Run();
-            //Example3.Run();
-            //await TestExample.RunAsync();
-            await TestExampleFixed.RunAsync();
+            var runMethod = new Action(Example1.Run);
+            runMethod();
+
+            //var runMethodAsync = new Func<Task>(TestExampleFixed.RunAsync);
+            //await runMethodAsync();
         }
     }
 }
