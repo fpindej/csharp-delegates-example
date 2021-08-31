81a1792607a4aecf3b7d9de4554cf9b7d32f9c60
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2021-08-31 04:42:07 +0200

    Implement anonymous method example

---
+++ AnonymousMethodExample.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DelegatesExample
+{
+    internal class AnonymousMethodExample
+    {
+        private delegate void NumberOperation(int n);
+
+        internal static void Run()
+        {
+            NumberOperation printPow = delegate(int x)
+            {
+                Console.WriteLine($"The pow of {x} is {x*x}");
+            };
+
+            printPow(5);            
+        }
+    }
+}
