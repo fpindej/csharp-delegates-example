042e9af1c188a872cbcd2a4b8cf51c79aa447339
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2021-08-31 04:52:45 +0200

    Implement multicast delegate example

---
+++ MulticastDelegateExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DelegatesExample
+{
+    internal class MulticastDelegateExample
+    {
+        private delegate void Calculate(int x, int y);
+
+        private static void Add(int x, int y) => Console.WriteLine($"The sum of {x} and {y} is {x + y}");
+
+        private static void Subtract(int x, int y) => Console.WriteLine($"The subtraction of {x} and {y} is {x - y}");
+
+        private static void Multiply(int x, int y) => Console.WriteLine($"The multiplication of {x} and {y} is {x * y}");
+
+        internal static void Run()
+        {
+            Calculate operation1 = Add;
+            Calculate operation2 = Subtract;
+            Calculate operation3 = Multiply;
+
+            Calculate calculate = operation1 + operation2 + operation3;
+            calculate(6, 9);
+
+            calculate -= operation2;
+            calculate(4, 20);
+        }
+    }
+}
